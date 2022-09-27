350ed4a71df985aa2c433dab72e8e99b302eae7d
azure928 <azure6.dev@gmail.com>
2022-09-27 10:36:14 +0900

    Update: 로그인 시 Refresh Token도 발급하도록 수정

--- components/user/userController.js
+++ components/user/userController.js
@@ -10,8 +10,14 @@ exports.signUp = async (req, res, next) => {
 };
 
 // 로그인
+/*
 exports.login = async (req, res, next) => {
   logger.info(`POST ${req.url}`);
   const token = await userService.login(req.body);
   res.status(200).json({ message: '로그인 성공', token });
+};*/
+
+exports.login = async (req, res, next) => {
+  const data = await userService.login(req.body);
+  res.status(200).json({ message: '로그인 성공', data });
 };
